885a4b80355c2dd54b0dcf538bab278b58b7ba12
brahmadpk <brahmadpk@gmail.com>
2016-02-07 19:04:34 +0000

    Swwep cache if source publsihed

--- app/sweepers/quote_sweeper.rb
+++ app/sweepers/quote_sweeper.rb
@@ -4,17 +4,17 @@ class QuoteSweeper < ActionController::Caching::Sweeper
     def after_update(quote)
         if quote.source_type == "Author"
             author = Author.find(quote.source_id)
-            expire_page(author_path(author))
+            expire_page(author_path(author)) if author.published
             quote.topics.each do |topic|
-                expire_page(topic_path(topic))
+                expire_page(topic_path(topic)) if topic.published
             end
         elsif quote.source_type == "Book"
             book = Book.find(quote.source_id)
             author = book.author
-            expire_page(book_path(book))
-            expire_page(author_path(author))
+            expire_page(book_path(book)) if book.published
+            expire_page(author_path(author)) if author.published
             quote.topics.each do |topic|
-                expire_page(topic_path(topic))
+                expire_page(topic_path(topic)) if topic.published
             end            
         end
     end
