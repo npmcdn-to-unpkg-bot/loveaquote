681f10c148ce392ea66025c3834a27b9d7ece73f
Deepak Kundu <brahmadpk@gmail.com>
2016-02-15 14:28:04 +0000

    Updated admin book show page

--- app/models/quote_topic.rb
+++ app/models/quote_topic.rb
@@ -2,6 +2,8 @@ class QuoteTopic < ActiveRecord::Base
     belongs_to :quote
     belongs_to :topic
     
+    validates_uniqueness_of :quote_id, scope: :topic_id
+    
     after_create :touch_topic
     
     def touch_topic
