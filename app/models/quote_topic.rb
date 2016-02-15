80dd09a34b8d3b6ec6c6231fd1b77fa8d79a1511
Deepak Kundu <brahmadpk@gmail.com>
2016-02-15 09:36:11 +0000

    Touch topic after quote topic acceptance

--- app/models/quote_topic.rb
+++ app/models/quote_topic.rb
@@ -1,4 +1,10 @@
 class QuoteTopic < ActiveRecord::Base
     belongs_to :quote
     belongs_to :topic
+    
+    after_create :touch_topic
+    
+    def touch_topic
+        self.topic.touch    
+    end
 end
