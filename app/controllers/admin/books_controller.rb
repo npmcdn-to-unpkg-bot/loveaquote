e7d2d4146c0a2a4c49b913f8b60cea2ca19f1e3d
Deepak Kundu <brahmadpk@gmail.com>
2016-03-04 17:43:08 +0000

    Added static pages

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -93,6 +93,6 @@ class Admin::BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:name, :fetch_url, :published, :popular, :very_popular, :book_id, :image, :character_ids => [])
+      params.require(:book).permit(:name, :fetch_url, :published, :popular, :very_popular, :book_id, :person_id, :image, :character_ids => [])
     end
 end
