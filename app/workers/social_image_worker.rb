25615a4dc41a219667c15e9c4cc1781110d616db
Deepak Kundu <brahmadpk@gmail.com>
2016-03-15 16:43:05 +0000

    Updated social image worker

--- app/workers/social_image_worker.rb
+++ app/workers/social_image_worker.rb
@@ -6,7 +6,29 @@ class SocialImageWorker
     source = class_name.constantize.find(id)
 
     if source
-      source_name = word_wrap(source.name + " " + "Quotes", line_width: 24)
+      two_lines = "Abraham Lincoln Abraham Lincoln Abraham Lincoln"
+      source_name = word_wrap(two_lines + " " + "Quotes", line_width: 22)
+      
+      source_line_count = source_name.split("\n").count
+      
+      case source_line_count
+      when 1
+        facebook_position = 260
+        google_plus_position = 202
+        twitter_position = 246
+      when 2
+        facebook_position = 195
+        google_plus_position = 144
+        twitter_position = 182
+      when 3
+        facebook_position = 132
+        google_plus_position = 86
+        twitter_position = 118
+      when 4
+        facebook_position = 68
+        google_plus_position = 28
+        twitter_position = 54
+      end
 
       #Generate Facebook Image
       facebook_image = Magick::Image.new(1200,627) {
@@ -19,10 +41,11 @@ class SocialImageWorker
 
       draw.pointsize = 108
       draw.gravity = Magick::CenterGravity
-      source_name.split("/n").each do |row|
-        draw.annotate(facebook_image, 1200, 627, 0, 0, row) {
+      source_name.split("\n").each do |row|
+        draw.annotate(facebook_image, 1000, 108, 100, facebook_position, row) {
           self.fill = "#FFFFFF"
         }
+        facebook_position += 128
       end
 
       #Generate Google Plus Image
@@ -34,15 +57,18 @@ class SocialImageWorker
       draw = Magick::Draw.new
       draw.font = "#{Rails.root}/app/assets/fonts/OpenSans-Regular.ttf"
 
-      draw.pointsize = 108
+      draw.pointsize = 96
       draw.gravity = Magick::CenterGravity
-      draw.annotate(google_plus_image, 0, 0, 0, 0, source_name) {
-        self.fill = "#FFFFFF"
-      }
+      source_name.split("\n").each do |row|
+        draw.annotate(google_plus_image, 1000, 108, 100, google_plus_position, row) {
+          self.fill = "#FFFFFF"
+        }
+        google_plus_position += 116
+      end
 
       #Generate Twitter Image
       twitter_image = Magick::Image.new(1200,600) {
-        self.background_color = "##4A525F"
+        self.background_color = "#4A525F"
         self.quality = 100
       }
 
@@ -51,9 +77,12 @@ class SocialImageWorker
 
       draw.pointsize = 108
       draw.gravity = Magick::CenterGravity
-      draw.annotate(twitter_image, 0, 0, 0, 0, source_name) {
-        self.fill = "#FFFFFF"
-      }
+      source_name.split("\n").each do |row|
+        draw.annotate(twitter_image, 1000, 108, 100, twitter_position, row) {
+          self.fill = "#FFFFFF"
+        }
+        twitter_position += 116
+      end
 
       require 'fileutils'
       Thread.new do
