f2e10711cc1a7763b79ea9ef2de688d0c0bdade2
Deepak Kundu <brahmadpk@gmail.com>
2016-03-16 07:57:27 +0000

    Added socialgenerated image to admin show

--- app/models/concerns/social_imageable.rb
+++ app/models/concerns/social_imageable.rb
@@ -3,5 +3,10 @@ module SocialImageable
     
     included do
         has_one :social_image, as: :source, dependent: :destroy
+        after_save :generate_social_image
+    end
+    
+    def generate_social_image
+        SocialImageWorker.perform_async(self.class.name, self.id)
     end
 end
