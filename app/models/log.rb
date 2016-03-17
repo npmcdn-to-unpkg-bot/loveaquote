df1616bcf2cec7483c8405a60477f92702d71784
Deepak Kundu <brahmadpk@gmail.com>
2016-03-17 19:28:02 +0000

    Added bingbot to log ignore list

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,8 +1,8 @@
 class Log < ActiveRecord::Base
     include PgSearch
-    pg_search_scope :search_by_description, against: :description, using: { tsearch: {prefix: true} }    
-    
+    pg_search_scope :search_by_description, against: :description, using: { tsearch: {prefix: true} }
+
     belongs_to :source, polymorphic: true
-    
-    BOTS = ["seokicks", "deusu"]
+
+    BOTS = ["seokicks", "deusu", "bingbot"]
 end
