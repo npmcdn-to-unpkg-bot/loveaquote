fcef9875cd1487019908c0dd71e7caba739594ce
Deepak Kundu <brahmadpk@gmail.com>
2016-03-17 05:44:16 +0000

    Clear time line cache on commit

--- app/models/time_line.rb
+++ app/models/time_line.rb
@@ -2,7 +2,7 @@ class TimeLine < ActiveRecord::Base
     belongs_to :item, polymorphic: true
     validates_uniqueness_of :item_id, scope: :item_type
     
-    after_create :expire_cache
+    after_commit :expire_cache
     
     def expire_cache
         Rails.cache.delete("time-line-recent")
