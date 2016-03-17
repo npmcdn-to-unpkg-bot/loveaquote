4a9cade084624255ce981c3f4b1beaa12c03d9a7
Deepak Kundu <brahmadpk@gmail.com>
2016-03-17 15:54:44 +0000

    Updated qotd cache

--- app/models/quote_of_the_day.rb
+++ app/models/quote_of_the_day.rb
@@ -1,9 +1,12 @@
 class QuoteOfTheDay < ActiveRecord::Base
   belongs_to :quote
-  
+
+  after_commit :expire_cache
+
   def self.today
-    Rails.cache.fetch("qotd-#{Date.today.to_s}") do
+    Rails.cache.fetch("qotd-#{Date.today.to_s(:number)}") do
         find_by(date: Date.today)
     end
   end
+  
 end
