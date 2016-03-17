9e6806a1a9a4c6a2fe2cedaeb1dbd46a270ff0eb
Deepak Kundu <brahmadpk@gmail.com>
2016-03-17 18:23:08 +0000

    Added ISBN to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317172952) do
+ActiveRecord::Schema.define(version: 20160317182014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160317172952) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.string   "image"
+    t.string   "isbn"
   end
 
   add_index "books", ["slug"], name: "index_books_on_slug", using: :btree
