2b52d5e835bfaa5489066a5713a4ed0fe5721014
Deepak Kundu <dpk@brahmadpk.com>
2016-03-19 23:50:31 +0530

    Removed tree from appl.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,22 +2,21 @@
 //= require jquery_ujs
 //= require foundation
 
-$(document).foundation();
-
 $(function(){
+  $(document).foundation();
   console.log("Working");
 });
 
 $(function() {
-    $(".share-quote-twitter").on("click", function(){
-        ga('send', 'event', 'Quote', 'Shared', 'Twitter');
-    });
+  $(".share-quote-twitter").on("click", function(){
+    ga('send', 'event', 'Quote', 'Shared', 'Twitter');
+  });
 
-    $(".share-quote-faceook").on("click", function(){
-        ga('send', 'event', 'Quote', 'Shared', 'Facebook');
-    });
+  $(".share-quote-faceook").on("click", function(){
+    ga('send', 'event', 'Quote', 'Shared', 'Facebook');
+  });
 
-    $(".share-quote-pinterest").on("click", function(){
-        ga('send', 'event', 'Quote', 'Shared', 'Pinterest');
-    })
+  $(".share-quote-pinterest").on("click", function(){
+    ga('send', 'event', 'Quote', 'Shared', 'Pinterest');
+  })
 });
