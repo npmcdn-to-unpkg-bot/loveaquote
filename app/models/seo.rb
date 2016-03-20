285f68dbb85ab50a2ae98dfb58bee085a40d8e65
Deepak Kundu <brahmadpk@gmail.com>
2016-03-20 20:06:37 +0000

    Updated topic seo

--- app/models/seo.rb
+++ app/models/seo.rb
@@ -1,5 +1,4 @@
 class Seo < ActiveRecord::Base
-  belongs_to :source, polymorphic: true
-  
+  belongs_to :source, polymorphic: true, touch: true
   validates_uniqueness_of :source_id, scope: :source_type
 end
