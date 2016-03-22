4f7badcbdf0154a28d34b2fecabe4ae6620cd3db
Deepak Kundu <brahmadpk@gmail.com>
2016-03-22 20:20:51 +0000

    Updated nginx conf

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.loveaquote.com"
+SitemapGenerator::Sitemap.default_host = "https://www.loveaquote.com"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
@@ -20,31 +20,31 @@ SitemapGenerator::Sitemap.create do
   #   add articles_path, :priority => 0.7, :changefreq => 'daily'
   #
   # Add all authors:
-  
+
   Person.published.each do |person|
     add person_path(person, format: :html), :lastmod => person.updated_at
   end
-  
+
   Book.published.each do |book|
     add book_path(book, format: :html), :lastmod => book.updated_at
   end
-  
+
   Movie.published.each do |movie|
     add movie_path(movie, format: :html), :lastmod => movie.updated_at
   end
-  
+
   TvShow.published.each do |tv_show|
     add tv_show_path(tv_show, format: :html), :lastmod => tv_show.updated_at
-  end  
-  
+  end
+
   Character.published.each do |character|
     add character_path(character, format: :html), :lastmod => character.updated_at
   end
-  
+
   Topic.published.each do |topic|
     add topic_path(topic, format: :html), :lastmod => topic.updated_at
   end
-  
+
   TopicCombination.all.each do |topic|
     add topics_topic_combination_path(topic, format: :html), :lastmod => topic.updated_at
   end
