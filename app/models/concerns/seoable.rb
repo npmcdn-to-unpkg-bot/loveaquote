3fdffe8812dfbbf5d289a1f38b5a1918200ada38
Deepak Kundu <brahmadpk@gmail.com>
2016-04-02 18:56:53 +0000

    Add redirects on slug change

--- app/models/concerns/seoable.rb
+++ app/models/concerns/seoable.rb
@@ -4,6 +4,7 @@ module Seoable
     included do
         has_one :seo, as: :source, dependent: :destroy
         accepts_nested_attributes_for :seo, reject_if: :all_blank
+        before_update :add_redirect
     end
 
     def title
@@ -49,6 +50,23 @@ module Seoable
         case self.class.name
         when "Topic"
             "Quotes about #{self.name}. Best collection of #{self.name} quotes brought to you by LoveAQuote. Enjoy reading these quotes on #{self.name}."
+        end
+    end
+    
+    def add_redirect
+        case self.class.name
+        when "Topic"
+            Redirect.create(from: "/topics/#{self.slug_was}", to: "/topics/#{self.slug}.html") if self.slug_changed?
+        when "Person"
+            Redirect.create(from: "/people/#{self.slug_was}", to: "/people/#{self.slug}.html") if self.slug_changed?
+        when "Book"
+            Redirect.create(from: "/books/#{self.slug_was}", to: "/books/#{self.slug}.html") if self.slug_changed?
+        when "Movie"
+            Redirect.create(from: "/movies/#{self.slug_was}", to: "/movies/#{self.slug}.html") if self.slug_changed?
+        when "TvShow"
+            Redirect.create(from: "/tv-shows/#{self.slug_was}", to: "/tv-shows/#{self.slug}.html") if self.slug_changed?
+        when "Character"
+            Redirect.create(from: "/characters/#{self.slug_was}", to: "/characters/#{self.slug}.html") if self.slug_changed?            
         end
     end
 end
