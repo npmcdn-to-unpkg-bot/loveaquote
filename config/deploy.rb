faaa459952d4526ea6c19a0f864f9e0ad0acd646
Deepak Kundu <brahmadpk@gmail.com>
2016-04-13 02:28:10 +0530

    Updated gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :stage, :production
 set :rbenv_type, :system
 set :rbenv_ruby, '2.2.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
 
 set :puma_init_active_record, true
 
