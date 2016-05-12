0042c4cf077c0bad831db460e11e3d9f82aa20c5
Deepak Kundu <brahmadpk@gmail.com>
2016-05-12 12:18:31 +0000

    Added lazyload to people

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require slick-carousel/slick/slick.js
+//= require jquery_lazyload/jquery.lazyload.js
 
 $(function() {
   $(".quote-images").slick({
     dots: true,
     arrows: false
   });
+  
+  $("img.lazy").lazyload();
   
   $(".share-quote-twitter").on("click", function(){
     window.open($(this).attr("data-url"), "_blank");
