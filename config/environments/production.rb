67359b99973b4b529e6a095d1f96b1e65bcad033
Deepak Kundu <brahmadpk@gmail.com>
2016-05-22 09:41:03 +0000

    Added user welcome mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = {host: "https://www.loveaquote.com"}
+  config.action_mailer.asset_host = "https://www.loveaquote.com"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.mailgun.org",
