1c0bf48d9c79008eb5336dbe5cd2063d9b720ad1
Deepak Kundu <brahmadpk@gmail.com>
2016-06-03 01:27:39 +0000

    Added reactivate account

--- app/controllers/my/dashboard_controller.rb
+++ app/controllers/my/dashboard_controller.rb
@@ -7,7 +7,12 @@ class My::DashboardController < ApplicationController
   
   def mark_for_deletion
     current_user.update_columns(mark_for_deletion: true)    
-    redirect_to my_root_url, alert: "Your account has been marked for deletion and will be removed soon. To reactivate, simply login again. Thankyou for your patronage."
+    redirect_to my_root_url, alert: "Your account has been marked for deletion and will be removed soon. To reactivate, simply login again. Thank you for your patronage."
   end
+  
+  def unmark_for_deletion
+    current_user.update_columns(mark_for_deletion: false)    
+    redirect_to my_root_url, notice: "Account reactivated. Thank you for staying with us."
+  end  
   
 end
