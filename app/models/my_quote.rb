df0a79e532f29d1ccd93e45faa43333ff7bb61bd
Deepak Kundu <brahmadpk@gmail.com>
2016-06-04 20:15:58 +0000

    Updated Navbar

--- app/models/my_quote.rb
+++ app/models/my_quote.rb
@@ -3,5 +3,4 @@ class MyQuote < ActiveRecord::Base
   belongs_to :quote
   
   has_many :my_quote_my_lists, dependent: :destroy
-  accepts_nested_attributes_for :quote_topics, reject_if: :all_blank, allow_destroy: true
 end
