971faac1da90cbcc5a28bae4e208cbe9f985fa09
Deepak Kundu <brahmadpk@gmail.com>
2016-06-06 13:55:52 +0000

    Added subscriber count to dashboard

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,5 @@
 class Subscriber < ActiveRecord::Base
     validates :email, presence: true, uniqueness: true, blank: false
     scope :unread, -> {where(read: false)}
+    scope :verified, -> {where(verified: true)}
 end
