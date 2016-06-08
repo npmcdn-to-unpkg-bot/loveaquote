9f9bd8d1a6ed59bcca999c222b1ff8051be54396
Deepak Kundu <brahmadpk@gmail.com>
2016-06-08 02:17:40 +0000

    Added quote share to ignore admin

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,9 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:pinterest, :facebook, :twitter, :google_plus]
 
   def twitter
-    QuoteTwitterWorker.perform_async(@quote.id, request.user_agent) if params[:human].present? && params[:human] == "t"
+    unless current_admin
+      QuoteTwitterWorker.perform_async(@quote.id, request.user_agent) if params[:human].present? && params[:human] == "t"
+    end  
     url = URI.encode(view_context.quote_source_url(@quote))
     text = CGI::escape(view_context.truncate(@quote.text, length: (140 - 22 - 23), seperator: " "))
 
@@ -10,13 +12,17 @@ class QuotesController < ApplicationController
   end
 
   def facebook
-    QuoteFacebookWorker.perform_async(@quote.id, request.user_agent) if params[:human].present? && params[:human] == "t"
+    unless current_admin
+      QuoteFacebookWorker.perform_async(@quote.id, request.user_agent) if params[:human].present? && params[:human] == "t"
+    end
     url = URI.encode(view_context.quote_source_url(@quote))
     redirect_to "https://www.facebook.com/sharer/sharer.php?u=#{url}"
   end
 
   def pinterest
-    QuotePinterestWorker.perform_async(@quote.id, request.user_agent) if params[:human].present? && params[:human] == "t"
+    unless current_admin
+      QuotePinterestWorker.perform_async(@quote.id, request.user_agent) if params[:human].present? && params[:human] == "t"
+    end
     url = URI.encode(view_context.quote_source_url(@quote))
     media = URI.encode(@quote.social_image.pinterest.url)
     description = CGI::escape(@quote.text + ' - ' + @quote.source.name +  ' #quotes')
